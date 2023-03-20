6e7d671e3196d22d5579f6bcdc2298367ef6007e
Mohamed Elhossiny <Mohamed.saed.elhossiny@gmail.com>
2023-03-20 20:47:51 +0200

    Multitasking delegate

--- Delegetes/Program.cs
+++ Delegetes/Program.cs
@@ -1,31 +1,50 @@
 ﻿namespace Delegetes
 {
+    
     internal class Program
     {
         static void Main(string[] args)
         {
-            var emps = new Employee[]
-            {
-                new Employee {Id=1, Name="Issam A", Gendr="M", TotalSales=65000m},
-                new Employee {Id=2,Name="Reem A",Gendr="F",TotalSales=50000m},
-                new Employee {Id=3,Name="Suzn A",Gendr="F",TotalSales=65000m},
-                new Employee {Id=4,Name="Nada A",Gendr="F",TotalSales=40000m},
-                new Employee {Id=5,Name="Ali A",Gendr="M",TotalSales=42000m},
-                new Employee {Id=6,Name="Hossam A",Gendr="M",TotalSales=30000m},
-                new Employee { Id = 7, Name = "Suzn A", Gendr = "F", TotalSales = 16000m },
-                new Employee { Id = 8, Name = "Suzn A", Gendr = "F", TotalSales = 15000m }
-            };
 
-            var report = new Report();
-            report.ProcessEmployee(emps, "Employee with Sales >= $60000 ", e => e.TotalSales >= 60000m) ;
-            report.ProcessEmployee(emps, "Employee with Sales $60000 > Sales > $30000 ", e => e.TotalSales >= 30000m && e.TotalSales < 60000m);
-            report.ProcessEmployee(emps, "Employee with Sales < $30000 ", e => e.TotalSales < 30000m);
+            var helper = new Rectangualrhelper();
+
+            // Calling Delegate
+            RecDelegate rect;
+
+            // join delegate to the method we want to functionate it
+            rect = helper.GetArea;
+            rect += helper.GetPreimeter;
+
+            rect(10,5);
+
+            Console.WriteLine("Removing GetPerimetre from Delegate");
+
+            rect -= helper.GetPreimeter;
+
+            rect(10, 5);
 
 
             Console.ReadKey();
 
         }
 
+    }
+
+    public delegate void RecDelegate(decimal width, decimal height);
+
+
+    public class Rectangualrhelper
+    {
+        public void GetArea(decimal width,decimal height)
+        {
+            var result = width * height;
+            Console.WriteLine($"Area: {width} x {height} = {result}");
+        }
+        public void GetPreimeter(decimal width, decimal height)
+        {
+            var result = 2 *(width + height);
+            Console.WriteLine($"Perimeter:2 x ({width} + {height}) = {result}");
+        }
     }
 
 }
